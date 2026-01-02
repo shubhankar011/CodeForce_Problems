0c813a84e1080c8793d793e6c2bc00091e4a23d6
Shubhankar Kumar <162623205+shubhankar011@users.noreply.github.com>
2026-01-03 03:17:14 +0530

    Refactor to use priority queue for efficiency

--- end_of_corruption.cpp
+++ end_of_corruption.cpp
@@ -1,22 +1,21 @@
+//FOR BEST Time Complexity of 0(log n)
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int n,m,input,max_val;
-    vector<int> vec;
+    priority_queue<int> vec;
     cin>>n>>m;
     int j = 0;
     vector<int> removed_elements;
     for(int i = 0; i < n+m; i++){
         cin>>input;
         if(input==-1){
-            auto res = minmax_element(vec.begin(), vec.end());
-                auto max_it = res.second;
-                removed_elements.push_back(*max_it);
-                vec.erase(max_it);
+           removed_elements.push_back(vec.top());
+           vec.pop();
         }
         else{
-            vec.push_back(input);
+            vec.push(input);
         }
     }
     for(auto c:removed_elements){
@@ -24,3 +23,32 @@ int main() {
     }
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n,m,input,max_val;
+//     vector<int> vec;
+//     cin>>n>>m;
+//     int j = 0;
+//     vector<int> removed_elements;
+//     for(int i = 0; i < n+m; i++){
+//         cin>>input;
+//         if(input==-1){
+//             auto res = minmax_element(vec.begin(), vec.end());
+//                 auto max_it = res.second;
+//                 removed_elements.push_back(*max_it);
+//                 vec.erase(max_it);
+//         }
+//         else{
+//             vec.push_back(input);
+//         }
+//     }
+//     for(auto c:removed_elements){
+//         cout<<c<<endl;
+//     }
+//     return 0;
+// }
+
+
